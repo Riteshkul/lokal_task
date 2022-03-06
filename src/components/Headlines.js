11660d1081293676686455dbb0b0f320f0a4783d
Riteshkul <riteshkul51@gmail.com>
2022-03-06 14:51:16 -0800

    Complete News App

--- src/components/Headlines.js
+++ src/components/Headlines.js
@@ -1,31 +1,40 @@
 import React from "react";
-import { Col, Container, Row,Card,Button } from "react-bootstrap";
+import { Col, Container, Row, Card, Button, Badge } from "react-bootstrap";
 
 const Headlines = (data) => {
-    console.log(data)
   return (
     <Container className="top-headlines">
-      <Row >
-          {
-              
-              data.data.map(item=>(
-                <Col className="headlines-cover" key={item.url} md={4}>
-                <Card style={{ width: "18rem" }}>
-                  <Card.Img variant="top" src={item.urlToImage} height="250px" />
-                  <Card.Body>
-                    <Card.Title>{item.title?item.title.slice(0,45)+"....":""} </Card.Title>
-                    <Card.Text>
-                    {item.description?item.description.slice(0,115)+"....":""}
-                    </Card.Text>
-                    <Button variant="primary" href={item.url} target="_blank">Read More</Button>
-                  </Card.Body>
-                </Card>
-              </Col>
-              ))
-          }
-        
-        </Row>
-       
+      <Row>
+        {data.data.map((item) => (
+          <Col className="headlines-cover" key={item.url} md={4}>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src={item.urlToImage} height="250px" />
+              <Card.Body>
+                <Badge bg="success">{item.source.name}</Badge>
+                <Card.Title>
+                  {item.title ? item.title.slice(0, 45) + "...." : ""}{" "}
+                </Card.Title>
+                <Card.Text>
+                  {item.description
+                    ? item.description.slice(0, 115) + "...."
+                    : ""}
+                  <p>
+                    <small className="text-muted">
+                      By{item.author ? " " + item.author : "  unknown"} on{" "}
+                      {item.publishedAt
+                        ? new Date(item.publishedAt).toGMTString()
+                        : "-"}
+                    </small>
+                  </p>
+                </Card.Text>
+                <Button variant="primary" href={item.url} target="_blank">
+                  Read More
+                </Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
     </Container>
   );
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,60 +1,71 @@
-import React,{useState,useEffect} from 'react';
-import Headlines from './Headlines'
-import Header from './Header';
-import { Container,Button } from 'react-bootstrap';
-const Home = ({ data, isLoading,category }) => {
-  const [items, setItems] = useState([])
-  const [articleCount,setarticleCount]=useState()
-    const [isloading, setIsLoading] = useState(true)
-  const [query, setQuery] = useState('')
-  let [page,setpageNo]=useState(1);
+import React, { useState, useEffect } from "react";
+import Headlines from "./Headlines";
+import Header from "./Header";
+import { Container, Button } from "react-bootstrap";
+const Home = ({ data, isLoading, category }) => {
+  const [items, setItems] = useState([]);
+  const [articleCount, setarticleCount] = useState();
+  const [isloading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState("");
+  let [page, setpageNo] = useState(1);
   useEffect(() => {
     const getbaddata = async () => {
-      setIsLoading(true)
-      const result = await fetch(`https://newsapi.org/v2/top-headlines?category=${category}&country=in&apiKey=a018bf892097482eab072784eed5dd84&q=${query}`)
-      const parsedresult=await result.json()
-      console.log(parsedresult)
-      setarticleCount(parsedresult.totalResults)
-      setItems(parsedresult.articles)
-      setIsLoading(false)
-    }
-    getbaddata()
-  }, [query])
-    console.log(data)
-    const nextpage=async()=>{
-      console.log("next")
-      const result = await fetch(`https://newsapi.org/v2/top-headlines?category=${category}&country=in&apiKey=a018bf892097482eab072784eed5dd84&q=${query}&page=${page+1}`)
-      const parsedresult=await result.json()
-      console.log(parsedresult.articles)
-      setItems(parsedresult.articles)
-      setIsLoading(false)
-      setpageNo(page+=1)
-    }
-    const previouspage=async()=>{
-      console.log("next")
-      const result = await fetch(`https://newsapi.org/v2/top-headlines?category=${category}&country=in&apiKey=a018bf892097482eab072784eed5dd84&q=${query}&page=${page-1}`)
-      const parsedresult=await result.json()
-      console.log(parsedresult.articles)
-      setItems(parsedresult.articles)
-      setIsLoading(false)
-      setpageNo(page-=1)
-    }
+      setIsLoading(true);
+      const result = await fetch(
+        `https://newsapi.org/v2/top-headlines?category=${category}&country=in&apiKey=a018bf892097482eab072784eed5dd84&q=${query}&pageSize=10`
+      );
+      const parsedresult = await result.json();
+
+      setarticleCount(parsedresult.totalResults);
+      setItems(parsedresult.articles);
+      setIsLoading(false);
+    };
+    getbaddata();
+  }, [query]);
+  const updateNews=async()=>{
+    const result = await fetch(
+      `https://newsapi.org/v2/top-headlines?category=${category}&country=in&apiKey=a018bf892097482eab072784eed5dd84&q=${query}&page=${
+        page 
+      }&pageSize=10`
+    );
+    const parsedresult = await result.json();
+    console.log(parsedresult.articles);
+    setItems(parsedresult.articles);
+    setIsLoading(false);
+  }
+  const nextpage = async () => {
+   
+    setpageNo((page += 1));
+    updateNews()
+  };
+  const previouspage = async () => {
+    setpageNo((page -= 1));
+    updateNews()
+  };
   return (
     <>
-    <Header getQuery={(q) => setQuery(q)}/>
-    {
-    isLoading ? (<h1>Loading</h1>)
-            :
-            <>
-            <Headlines isLoading={isloading} data={items}/>
-            <Container className='d-flex justify-content-between my-3'>
-              <Button disabled={page<=1} variant="dark" onClick={previouspage}>&larr; Previous</Button>
-              <Button disabled={page>=Math.ceil(articleCount/20)}variant="dark" onClick={nextpage}>Next &rarr;</Button>
-            </Container>
-            </>
-    }
-            </>
-  )
-}
+      <Header getQuery={(q) => setQuery(q)} />
+      {isLoading ? (
+        <h1>Loading</h1>
+      ) : (
+        <>
+          <Headlines isLoading={isloading} data={items} />
+          <Container className="d-flex justify-content-between my-3">
+            <Button disabled={page <= 1} variant="dark" onClick={previouspage}>
+              &larr; Previous
+            </Button>
+            <Button
+              disabled={page >= Math.ceil(articleCount / 10)}
+              variant="dark"
+              onClick={nextpage}
+            >
+              Next &rarr;
+            </Button>
+          </Container>
+        </>
+      )}
+    </>
+  );
+};
 
-export default Home
+export default Home;
